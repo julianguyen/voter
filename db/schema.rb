f484813ac8f2bdefb1ffccede283bc878e938632
Esther j <jesther0103@gmail.com>
2014-01-07 15:03:45 -0500

    added rspec stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140106192341) do
+ActiveRecord::Schema.define(:version => 20140107195955) do
 
   create_table "answers", :force => true do |t|
     t.integer  "questionid"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(:version => 20140106192341) do
 
   create_table "questions", :force => true do |t|
     t.string   "question"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "votes", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
---
+++ spec/charCount_spec.rb
@@ -0,0 +1,8 @@
+require  "spec_helper"
+
+describe "Question" do
+		it "should have at least 1 character" do
+			Question.create(question: "a")
+			expect(Question.where(:question => "a").exists?).to eq(true)
+	end
+end
