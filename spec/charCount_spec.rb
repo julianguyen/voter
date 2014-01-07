522a9ef8a4081fbb8689f571d427877a3ea6020c
Esther j <jesther0103@gmail.com>
2014-01-07 15:52:49 -0500

    Tests fixed

--- spec/charCount_spec.rb
+++ spec/charCount_spec.rb
@@ -1,8 +1,17 @@
 require  "spec_helper"
 
-describe "Question" do
-		it "should have at least 1 character" do
-			Question.create(question: "a")
-			expect(Question.where(:question => "a").exists?).to eq(true)
+describe Question do
+	it "have character count between 1 and 250" do
+		Question.create(question: "a")
+		expect(Question.where(:question => "a").exists?).to eq(true)
+	end
+	it "should fail character count less than 1" do
+		Question.create(question: "")
+		expect(Question.where(:question => "").exists?).to eq(false)
+	end
+ 	it "should fail character count greater than 250" do
+		Question.create(question: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		expect(Question.where(:question => "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").exists?).to eq(false)
 	end
 end
+
