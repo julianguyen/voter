3c6657edf858f50f9f07e34063383156c8cf7f2d
Esther j <jesther0103@gmail.com>
2014-01-08 12:03:48 -0500

    Rspec for voting

--- spec/controllers/submitWithoutVoting_spec.rb
+++ spec/controllers/submitWithoutVoting_spec.rb
@@ -1,18 +1,20 @@
 require  "spec_helper"
 
+
 describe VotesController do
 	describe "Vote message" do
-		it "Should acknowledge vote submission" do 
+		before(:each) do
 			theid = Question.create(question: "What time is it?")
-			ans1 = Answer.create(questionid: theid, answer: "5 pm")
-			ans2 = Answer.create(questionid: theid, answer: "6 pm")
-			post :voting, {:ans => ans1}
+            @ans2 = Answer.create(questionid: theid, answer: "6 pm")
+            @ans1 = Answer.create(questionid: theid, answer: "5 pm")
 		end
-		 it "Should not acknowledge vote submission" do
-		 	theid = Question.create(question: "What time is it?")
-		 	ans1 = Answer.create(questionid: theid, answer: "5 pm")
-		 	ans2 = Answer.create(questionid: theid, answer: "6 pm")
-		 	post :voting, {:ans => 94983}
+		it "Should acknowledge vote submission" do 
+			post :voting, {:ans => @ans1}
+			response.should redirect_to('/votes/voted')
+		end
+		 it "Should indicate error when vote was not speicifed" do
+		 	post :voting
+		 	response.should redirect_to('/votes/vote')
 		 end
 	end
 end
